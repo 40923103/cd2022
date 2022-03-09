a4066519460f0246c65d1c803096f00052b0c7a4
40923103 <40923103@gm.nfu.edu.tw>
2022-03-09 10:02:51 +0800

    fork

--- content/tipuesearch_content.js
+++ content/tipuesearch_content.js
@@ -1,1 +1,1 @@
-var tipuesearch = {"pages": [{'title': 'About', 'text': 'cmsimde_site', 'tags': '', 'url': 'About.html'}]};
+var tipuesearch = {"pages": [{'title': 'About', 'text': 'cmsimde_site \n', 'tags': '', 'url': 'About.html'}, {'title': 'note', 'text': '', 'tags': '', 'url': 'note.html'}, {'title': 'git clone', 'text': '1235678 \n', 'tags': '', 'url': 'git clone.html'}, {'title': 'token', 'text': '11222 \n', 'tags': '', 'url': 'token.html'}, {'title': 'pull request', 'text': '\n 點選右上角fork \n \n 點選 Add file \xa0  Upload files \n', 'tags': '', 'url': 'pull request.html'}]};
